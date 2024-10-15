22be1a9c156ffce9cb98f4fed3bc65d6b4424021
rjam480 <rj480@hotmail.com>
2024-10-15 11:32:49 -0500

    usuarios validacion existencia

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -19,17 +19,26 @@ class AuthController extends Controller
         $password = $request->get('password');
       
         $user = $this->user->where('nit','=',$nit)->first();
-        $validandoHas = \Hash::check($password,$user->password);
-       
-        
-        if ($validandoHas) {
-            $token = $user->createToken($user->name.'-AuthToken')->plainTextToken;
-            return response()->json([
-                'data'          => $user,
-                'access_token'  => $token,
-                'token_type'    => 'Bearer',
-                'msg'           => 'Login'
-            ]);
+
+        if ($user) {
+            $validandoHas = \Hash::check($password,$user->password);
+           
+            if ($validandoHas) {
+                $token = $user->createToken($user->name.'-AuthToken')->plainTextToken;
+                return response()->json([
+                    'data'          => $user,
+                    'access_token'  => $token,
+                    'token_type'    => 'Bearer',
+                    'msg'           => 'Login'
+                ]);
+            }else{
+                return response()->json([
+                    'data'          => [],
+                    'access_token'  => '',
+                    'token_type'    => '',
+                    'msg'           => 'Credenciales incorrectas'
+                ]);
+            }
         }else{
             return response()->json([
                 'data'          => [],
