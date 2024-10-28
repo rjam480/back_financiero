c42502d004549294432158c830047cbe2b6dae1c
rjam480 <rj480@hotmail.com>
2024-10-28 14:17:20 -0500

    ajuste en el template del correo creacion cuenta

--- resources/views/emails/creacion_cuenta.blade.php
+++ resources/views/emails/creacion_cuenta.blade.php
@@ -50,8 +50,6 @@
                                         <p style="color:#455056; font-size:15px;line-height:24px; margin:0;">
                                             Password: {{$data['password']}}
                                         </p>
-                                        <a href="javascript:void(0);"
-                                            style="background:#20e277;text-decoration:none !important; font-weight:500; margin-top:35px; color:#fff;text-transform:uppercase; font-size:14px;padding:10px 24px;display:inline-block;border-radius:50px;">Recuperar contraseña</a>
                                     </td>
                                 </tr>
                                 <tr>
